e2c52f6975bc74d886312c6a6b8f8da55db5071f
Rielly Luiza <riellyduarte75@gmail.com>
2026-08-26 18:13:34 +0000

    exercicio da pagina 210

---
+++ atividade-um/app/pg210/page.js
@@ -0,0 +1,9 @@
+import styles from "./pg210.module.css";
+
+export default function pg210() {
+  return (
+    <div className= {styles.container}>
+      <h1 className = {styles.titulo}>Primeiro exercício de CSS</h1>
+    </div>
+  );
+}
